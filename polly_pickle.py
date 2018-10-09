b2147a5b4a88c99df896289b690c97ff2fa1e3c6
Mike McGarry <mikemcgarry@live.com.au>
2018-10-09 12:09:29 +0100

    refactored code and added better documentation

--- polly_pickle.py
+++ polly_pickle.py
@@ -1,4 +1,8 @@
-#Polygraph
+'''
+This is used to train your NLP model before pickling it and uploading it 
+into an S3 bucket. Next step is to build in reinforcement learning and to deploy
+as a lambda function instead of on an instance
+''''
 
 #importing the libraries
 #http://pandas.pydata.org/
@@ -17,6 +21,8 @@ from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import confusion_matrix
 from sklearn.cross_validation import train_test_split
+from sklearn.externals import joblib
+from sklearn.pipeline import Pipeline
 
 #Step 1: Importing Data
 #importing the dataset using pandas
@@ -25,29 +31,44 @@ def import_dataset(dataset_name, file_type, **kwargs):
 	  return dataset
 
 #Step 1: Importing Data
-dataset_name = 'NLP824 All.tsv'
+#The name of the file with your dataset goes here, the file should have the
+#text in the first column and the lable in the second column      
+dataset_name = ''
+#The format of your dataset goes here
 dataset_type = 'csv'
+#Import the dataset
 dataset = import_dataset(dataset_name, dataset_type)
-
+#Create porter stemmer for stemming words
 ps = PorterStemmer()
 #Cleaning the text
+
 def clean_text(raw_text):    
+    #Ensure that all words have a single space between them
     cleaned_text = re.sub('[^a-zA-Z0-9]', ' ', raw_text)
+    #Make all the text lower case
     cleaned_text = cleaned_text.lower()
+    #Split the test into a list of words
     cleaned_text = cleaned_text.split()
+    #Remove stopwords and call stem on all other words
     cleaned_text = [ps.stem(word) for word in cleaned_text if not word in set(stopwords.words('english'))]
+    #Reconnect the text together
     cleaned_text = ' '.join(cleaned_text)
-    
+    #Return the cleaned text
     return cleaned_text
 
+#Initialise the corpus
 corpus = []
 
+#Fill the corpus with cleaned text from the dataset
 for i in range(len(dataset)):
     cleaned_text = clean_text(dataset[0][i])    
     corpus.append(cleaned_text)
-    
+
+#Reduce the number of features
 cv = CountVectorizer(max_features = 1500)
 x = cv.fit_transform(corpus).toarray()
+
+#Break off target label (second column in dataset)
 y = dataset.iloc[:,1].values
     
 #Step 5: Split into training/test sets 
@@ -58,10 +79,7 @@ from sklearn.ensemble import RandomForestClassifier
 classifier = RandomForestClassifier(n_estimators=25, random_state=3, criterion="entropy")
 classifier.fit(x_train, y_train)
 
-
-from sklearn.externals import joblib
-from sklearn.pipeline import Pipeline
-
+#Pickle the model for storage in S3 to be reloaded by the classifier
 polygraphpipe = Pipeline([('cv', cv), ('classifier', classifier)])
 joblib.dump(polygraphpipe, 'polygraphpipe.pkl', protocol = 0)
 
--- predictions.py
+++ predictions.py
@@ -1,3 +1,8 @@
+'''
+File to be deployed and run with nohup on an AWS Ubuntu instance with a Conda
+Environment
+'''
+
 from sklearn.externals import joblib
 from boto.s3.key import Key
 from boto.s3.connection import S3Connection, Location
@@ -6,54 +11,101 @@ from flask import Flask, jsonify, abort, request
 from nltk.stem.porter import PorterStemmer
 import re
 import urllib
-BUCKET_NAME = ''
-MODEL_FILE_NAME = ''
-MODEL_LOCAL_PATH = '/tmp/' + MODEL_FILE_NAME
-stopwords_raw = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn']
 
+#The name of the S3 bucket where your pickled model file is
+BUCKET_NAME = ''
+#The name of the pickled model file inside the S3 bucket
+MODEL_FILE_NAME = ''
+#The path on the local instance to save the file to
+MODEL_LOCAL_PATH = '/tmp/' + MODEL_FILE_NAME
+#Raw stopwords to remove, in raw form as problematic to download on AWS instance
+stopwords_raw = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
+                 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him',
+                 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its',
+                 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+                 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
+                 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been',
+                 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
+                 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because',
+                 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about',
+                 'against', 'between', 'into', 'through', 'during', 'before',
+                 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in',
+                 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then',
+                 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all',
+                 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some',
+                 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than',
+                 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should',
+                 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
+                 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma',
+                 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren',
+                 'won', 'wouldn']
+
+#App to handle the API requests
 app = Flask(__name__)
 
+#Check that the API is live
 @app.route('/')
 def index():
     return "We are live"
     
+#Returns the prediction from a GET request with the email body 
 @app.route('/polygraph', methods=['GET'])
 def result(*args):
+    #Collect the arguments from the GET request
     arguments = request.args
     
     try:
+        #Try and collect the response argument
         response = str(arguments['response'])
     except:
+        #If it does not exist abort
         abort(404)    
-
+    
+    #predict the label based on the email response
     y_pred = predict(response)
     
-    
+    #return the prediction
     return str(y_pred[0])
 
 
 #Cleaning the text
 def clean_text(raw_text): 
+    #Create porter stemmer for stemming words
     ps = PorterStemmer()
+    #Ensure that all words have a single space between them
     cleaned_text = re.sub('[^a-zA-Z0-9]', ' ', raw_text)
+    #Make all the text lower case
     cleaned_text = cleaned_text.lower()
+    #Split the test into a list of words
     cleaned_text = cleaned_text.split()
+    #Remove stopwords and call stem on all other words
     cleaned_text = [ps.stem(word) for word in cleaned_text if not word in set(stopwords_raw)]
+    #Reconnect the text together
     cleaned_text = ' '.join(cleaned_text)
-    
+    #Return the cleaned text
     return cleaned_text
 
+#Loads the pickled NLP model
 def load_model():
-  conn = boto.connect_S3()
-  bucket = conn.get_bucket(BUCKET_NAME)
-  key_obj = Key(bucket)
-  key_obj.key = MODEL_FILE_NAME
+    #open an S3 connection
+    conn = boto.connect_S3()
+    #connect to the bucket with our pickled model
+    bucket = conn.get_bucket(BUCKET_NAME)
+    #create a key object
+    key_obj = Key(bucket)
+    #connect to the pickled model file
+    key_obj.key = MODEL_FILE_NAME
+    #save model to local filesystem
+    contents = key_obj.get_contents_to_filename(MODEL_LOCAL_PATH)
+    #load and return the pickled model
+    return joblib.load(MODEL_LOCAL_PATH)
 
-  contents = key_obj.get_contents_to_filename(MODEL_LOCAL_PATH)
-  return joblib.load(MODEL_LOCAL_PATH)
-
+#Predicts the label for the email
 def predict(data):
   final_formatted_data = []
+  #Decode the URL encoded response
   response_decoded = urllib.unquote_plus(data)
+  #Clean the response
   final_formatted_data.append(clean_text(response_decoded))
+  #Return the prediction using the loaded model
   return load_model().predict(final_formatted_data)
